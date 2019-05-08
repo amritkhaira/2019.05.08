040472825aa96df8e363b7f1a3a1033d8be5516c
Amritpal Singh <707763@student.tor.loc>
2019-05-08 11:55:38 -0400

    11:55

---
+++ C0707763.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// first program to add numnbers by stepping down
+
+            int sum = 0;
+            int start = 314;
+            int end = 102;
+            for (int i = start; i <= end; i--)
+            {
+                Console.WriteLine(end + "" + i);
+
+                sum += i;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
